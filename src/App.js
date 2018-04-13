b8f62396254f6bb4230097ce9702f34bdc3dd3cb
Roman Babanov <r.babanov@bi.zone>
2018-04-13 16:48:22 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
+
+import './global.css';
+
+import HomePage from './pages/Home/index';
+import PageA from './pages/pageA/index';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      routes: [
+        {
+          title: 'Home',
+          key: 'home',
+          link: '/',
+          component: HomePage
+        },
+        {
+          title: 'PageA',
+          key: 'page-a',
+          link: '/page-a',
+          component: PageA
+        }
+      ]
+    };
+  }
+
+  renderMenuItems() {
+    const state = this.state;
+    const { routes } = state;
+
+    return routes.map(route => {
+      return (
+        <Link to={route.link} key={route.key}>
+          <div className="menu-item">{route.title}</div>
+        </Link>
+      );
+    });
+  }
+
+  renderRoutes() {
+    const state = this.state;
+    const { routes } = state;
+
+    return routes.map(route => {
+      const routeKey = `${route.key} ${route.title}`;
+      return <Route exact key={routeKey} path={route.link} component={route.component}/>;
+    });
+  }
+
   render() {
     return (
-      <h1>Hello world</h1>
+      <div className="layout">
+        <div className="nav-menu">
+          {this.renderMenuItems()}
+        </div>
+        <div className="page-container">
+          <Switch>
+            {this.renderRoutes()}
+          </Switch>
+        </div>
+      </div>
     );
   }
 };
---
+++ src/pages/pageA/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PageA extends Component {
+  render() {
+    return (
+      <h1>Page A</h1>
+    );
+  }
+};
