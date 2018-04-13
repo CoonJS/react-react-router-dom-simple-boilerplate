a1d5a6f8084d10463e3cf3d8cd33c2ae3c1df04e
Roman Babanov <r.babanov@bi.zone>
2018-04-13 16:20:08 +0300

    init project

---
+++ README.md
@@ -0,0 +1,22 @@
+# Basic React, React-router-dom boilerplate for starting projects with webpack4
+
+### start
+
+```
+npm install
+npm run start
+```
+
+> Browser open automatically, If not, then try to open http://localhost:8080/ in your browser
+
+### production
+
+```
+npm run build
+```
+
+> You will have your production code in `dist` directory
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var entryPoint = path.resolve(__dirname, 'index.js');
+
+module.exports = {
+  entry: entryPoint,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=2'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(woff2?)$/,
+        loader: 'file-loader'
+      }
+
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React, React-router-dom simple boilerplate',
+      template: 'index.html',
+      filename: 'index.html'
+    })
+  ]
+};
