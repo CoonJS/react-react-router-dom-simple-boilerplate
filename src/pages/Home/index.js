febd76833b412285fe19261ed1fdb4a2ee738776
Marat Zh <regeon-93@inbox.ru>
2018-04-17 18:08:08 +0300

    add css for Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+import './index.css';
+
 export default class HomePage extends Component {
   render() {
     return (
-      <h1>Home Page</h1>
+      <div className="flex-container">
+      <div className="flex-element">1</div>
+      <div className="flex-element">
+        <div className="flex-element-ch">1</div>
+        <div className="flex-element-ch">2</div>
+        <div className="flex-element-ch">3</div> 
+        <div className="flex-element-ch">4</div>
+      </div>
+      <div className="flex-element">
+        <div className="flex-element-ch">1</div>
+        <div className="flex-element-ch">2</div>
+      </div>      
+    </div> 
     );
   }
 };
