7a80d18a1355c361acc4f89152ced8d096e57450
Roman Babanov <r.babanov@bi.zone>
2018-11-28 01:58:49 +0300

    update text

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class HomePage extends Component {
   render() {
     return (
-      <h1>Home Page</h1>
+      <h1>Home Page powered by React</h1>
     );
   }
 };
